b812e06c513533232c5b9973cb114d80a7310231
Corey Fields <corey.fields@airspacelink.com>
2021-03-18 09:40:31 -0400

    no message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+
+const { Header, Content } = Layout;
+/* GET THESE FROM NOSQL TABLE */ const types = ['Gumdrop', 'Hard Candy'];
+
+const typeMenu = (
+  <Menu>
+    {types.map((type, i) => (
+      <Menu.Item key='0'>{type}</Menu.Item>
+    ))}
+  </Menu>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Layout className='layout'>
+        <Header>
+          <div className='logo' />
+          <Menu theme='dark' mode='horizontal'>
+            <Menu.Item key='1'>Recipes</Menu.Item>
+            <Menu.Item key='2'>Orders</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Dropdown overlay={typeMenu} trigger={['click']}>
+            <a className='ant-dropdown-link' onClick={e => e.preventDefault()}>
+              Type <DownOutlined />
+            </a>
+          </Dropdown>
+        </Content>
+      </Layout>
     </div>
   );
 }
---
+++ src/fire.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyAtUeefYJRJfU9UFkbE3y26F6AWdFvOxQ8",
+  authDomain: "subculture-labs-c2085.firebaseapp.com",
+  databaseURL: "https://subculture-labs-c2085-default-rtdb.firebaseio.com",
+  projectId: "subculture-labs-c2085",
+  storageBucket: "subculture-labs-c2085.appspot.com",
+  messagingSenderId: "388779378862",
+  appId: "1:388779378862:web:5de76e132243ba1cdd453f",
+  measurementId: "G-MG38R5P7VG"
+};
+
+const fire = firebase.initializeApp(config);
+
+export default fire;
